ca1b42d3358c2f9c07b25ba2f265919699b1570c
Howard <walterhowardanti@outlook.com>
2024-02-16 13:18:27 -0400

    almost

---
+++ diagnistico-inf-133/src/app/components/PokemonCard.js
@@ -0,0 +1,94 @@
+"use client"
+import React from "react";
+import styles from './PokemonCard.module.css';
+import { useState, useEffect } from "react";
+
+function PokemonCard({pokenumber}){
+
+    const url="https://pokeapi.co/api/v2/pokemon/"+pokenumber;
+
+    const [pokemon,setPokemon]=useState({});
+    const [pokeabouts,setPokeabouts]=useState([]);
+    const [pokestats,setPokestats]=useState([]);
+    const [poketypes,setPoketypes]=useState([]);
+    const [pokeabilities,setPokeabilities]=useState([]);
+
+
+    useEffect(()=>{
+        fetch(url).then(res=>res.json()).then(data=>{
+            setPokemon(
+                {id: data.id, name:data.name, image:data.sprites.other.dream_world.front_default}
+            ),
+            setPokeabouts([
+                {parameter: "height", value: data.height/10+" m"},
+                {parameter: "weight", value: data.weight/10+" kg"},
+            ]),
+            setPokestats(data.stats),
+            setPoketypes(data.types),
+            setPokeabilities(data.abilities)
+        })
+    })
+
+    return(
+        <div className={styles.maincontainer} >
+            <div className={styles.titles} >
+                <span className={styles.subtitle} >My Pokemon</span>
+                <span className={styles.maintitle} >{pokemon.name}</span>
+            </div>
+            <div className={styles.pokeimage} >
+                <span className={styles.id} >{"#"+pokemon.id}</span>
+                <figure>
+                    <img src={pokemon.image} />
+                </figure>
+            </div>
+            <div className={styles.pokeinfo} >
+                <span className={styles.stat} >About</span>
+                <div className={styles.horizontal} ></div>
+                <div className={styles.linecontainer} >
+                    <div className={styles.infoline} >
+                        <span className={styles.parameter} >Types</span>
+                        <div className={styles.items} >
+                        {poketypes.map((poketype,index)=>{
+                            return(
+                                <span key={index} className={styles.valuet} >{poketype.type.name}</span>
+                            )
+                        })}
+                        </div>
+                    </div>
+                    {pokeabouts.map(pokeabout=>{
+                        return(
+                            <div key={pokeabout.id} className={styles.infoline} >
+                                <span className={styles.parameter} >{pokeabout.parameter}</span>
+                                <span className={styles.value} style={{textTransform: 'lowercase'}} >{pokeabout.value}</span>
+                            </div>
+                        )
+                    })}
+                    <div className={styles.infoline} >
+                        <span className={styles.parameter} >Abilities</span>
+                        <div className={styles.items} >
+                        {pokeabilities.map((pokeability,index)=>{
+                            return(
+                                <span key={index} className={styles.valuet} >{pokeability.ability.name}</span>
+                            )
+                        })}
+                        </div>
+                    </div>
+                </div>
+                <span className={styles.stat} >Stats</span>
+                <div className={styles.linecontainer} >
+                    {pokestats.map((pokestat,index)=>{
+                        return(
+                            <div key={index} className={styles.infoline} >
+                                <span className={styles.parameter} >{pokestat.stat.name}</span>
+                                <span className={styles.value} >{pokestat.base_stat}</span>
+                            </div>
+                            
+                        )
+                    })}
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default PokemonCard;
---
+++ diagnistico-inf-133/src/app/components/PokemonList.js
@@ -0,0 +1,33 @@
+"use client"
+
+import React, { useState,useEffect } from "react";
+import styles from './PokemonList.module.css'
+import PokemonCard from "./PokemonCard";
+
+function PokemonList({number}){
+
+    const [pokemons,setPokemons]=useState([]);
+    const url="https://pokeapi.co/api/v2/pokemon?offset=0&limit="+number*5;
+
+    useEffect(()=>{
+        fetch(url).then(res=>res.json()).then(data=>{
+            setPokemons(data.results);
+        })
+    },[])
+
+
+
+    return(
+        <div className={styles.maincontainer} >
+            {pokemons.map((pokemon,index)=>{
+                if((index+1)%5==0){
+                    return(
+                        <PokemonCard key={index} pokenumber={pokemon.name} />
+                    )
+                }
+            })}
+        </div>
+    )
+}
+
+export default PokemonList;
